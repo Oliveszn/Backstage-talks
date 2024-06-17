dbba808faec61bd815c9b683397ba32a51e0544f
oliveszn <ekpealamicheal@gmail.com>
2024-06-17 05:15:05 +0100

    initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div class="header">
+      <img src="logo.png" alt="Logo" className="h-auto w-64" />
+    </div>
+  );
+};
+
+export default Navigation;
